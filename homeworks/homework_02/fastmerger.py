2058d8e9555fa92955aa58d045434bff56520757
Макаров Максим <MisterMaks97@mail.ru>
2019-03-14 11:51:48 +0300

    ДЗ_2 Доп. задача 1

--- homeworks/homework_02/fastmerger.py
+++ homeworks/homework_02/fastmerger.py
@@ -12,4 +12,13 @@ class FastSortedListMerger:
         принимает на вход список отсортированных непоубыванию списков и число
         на выходе выдает один список длинной k, отсортированных по убыванию
         '''
-        raise NotImplementedError
+        true_lst = []
+        lst = []
+        for small_list in list_of_lists:
+            for number in small_list:
+                lst.append((number, len(lst)))
+        instance_maxheap = MaxHeap(lst)
+        for i in range(k):
+            true_lst.append(instance_maxheap.extract_maximum()[0])
+        return true_lst
+        # raise NotImplementedError
--- homeworks/homework_02/heap.py
+++ homeworks/homework_02/heap.py
@@ -3,24 +3,61 @@
 
 
 class Heap():
-
     def __init__(self, array):
-        pass
+        self.array = array[:]
+        self.build_heap()
+        # pass
 
     def add(self, elem_with_priority):
-        pass
+        self.array.append(elem_with_priority)
+        self.sift_up()
+        # pass
 
     def build_heap(self):
-        pass
+        for i in range(len(self.array) // 2 - 1, -1, -1):
+            self.sift_down(i)
+            # pass
+
+    def sift_up(self):
+        index = len(self.array) - 1
+        while index > 0:
+            parent = (index - 1) // 2
+            if comparator_d(self.array[parent], self.array[index]):
+                return True
+            self.array[index], self.array[parent] = \
+                self.array[parent], self.array[index]
+            index = parent
+
+    def sift_down(self, i):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        largest = i
+        if left < len(self.array) and \
+                comparator_d(self.array[left], self.array[i]):
+            largest = left
+        if right < len(self.array) and \
+                comparator_d(self.array[right], self.array[largest]):
+            largest = right
+        if largest != i:
+            self.array[i], self.array[largest] = \
+                self.array[largest], self.array[i]
+            self.sift_down(largest)
 
 
 class MaxHeap(Heap):
-
     def __init__(self, array):
-        raise NotImplementedError
+        super().__init__(array)
+        # raise NotImplementedError
 
     def extract_maximum(self):
-        pass
+        maximum = 0
+        if len(self.array) > 0:
+            maximum = self.array.pop(0)
+            if len(self.array) > 0:
+                self.array.insert(0, self.array.pop())
+                self.sift_down(0)
+        return maximum
+        # pass
 
 
 def comparator_d(x, y):
