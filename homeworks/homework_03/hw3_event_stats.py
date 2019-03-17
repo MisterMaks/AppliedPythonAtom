a987e352bacfaed85592306c19c465688412ef7f
Макаров Максим <MisterMaks97@mail.ru>
2019-03-18 02:34:39 +0300

    ДЗ 3 Активность пользователя в скользящем окне

--- homeworks/homework_03/hw3_event_stats.py
+++ homeworks/homework_03/hw3_event_stats.py
@@ -7,7 +7,8 @@ class TEventStats:
 
     def __init__(self):
         # TODO: реализовать метод
-        raise NotImplementedError
+        self.dict_users = {}
+        # raise NotImplementedError
 
     def register_event(self, user_id, time):
         """
@@ -17,7 +18,15 @@ class TEventStats:
         :return: None
         """
         # TODO: реализовать метод
-        raise NotImplementedError
+        if self.dict_users.get(user_id):
+            if self.dict_users[user_id].get(time):
+                self.dict_users[user_id][time] += 1
+            else:
+                self.dict_users[user_id][time] = 1
+        else:
+            self.dict_users[user_id] = {time: 1}
+        return None
+        # raise NotImplementedError
 
     def query(self, count, time):
         """
@@ -29,4 +38,15 @@ class TEventStats:
         :return: activity_count: int
         """
         # TODO: реализовать метод
-        raise NotImplementedError
+        activity_count = 0
+        if count == 0:
+            return activity_count
+        for user_key in self.dict_users.keys():
+            count_event_of_user = 0
+            for time_key in self.dict_users[user_key].keys():
+                if time_key > time - self.FIVE_MIN and time_key <= time:
+                    count_event_of_user += self.dict_users[user_key][time_key]
+            if count_event_of_user == count:
+                activity_count += 1
+        return activity_count
+        # raise NotImplementedError
