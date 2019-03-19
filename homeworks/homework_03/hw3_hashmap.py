4398a16277cc7a349560adac4df082c86afb7446
Макаров Максим <MisterMaks97@mail.ru>
2019-03-20 00:35:41 +0300

    ДЗ 3 HashMap

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -7,6 +7,7 @@ class HashMap:
     Давайте сделаем все объектненько,
      поэтому внутри хешмапы у нас будет Entry
     '''
+
     class Entry:
         def __init__(self, key, value):
             '''
@@ -14,69 +15,171 @@ class HashMap:
             :param key: ключ
             :param value: значение
             '''
+            self.key = key
+            self.value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
+            # raise NotImplementedError
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
+            # raise NotImplementedError
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.key
+            # raise NotImplementedError
 
     def __init__(self, bucket_num=64):
         '''
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         '''
-        raise NotImplementedError
+        self.buckets = [[] for _ in range(bucket_num)]
+        self.size = bucket_num
+        self.count = 0
+        # raise NotImplementedError
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        hash_key = self._get_hash(key)
+        index = self._get_index(hash_key)
+        for element in self.buckets[index]:
+            if element.get_key() == key:
+                return element.get_value()
+        return default_value
+        # raise NotImplementedError
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        hash_key = self._get_hash(key)
+        index = self._get_index(hash_key)
+        for element in self.buckets[index]:
+            if element.get_key() == key:
+                element.value = value
+                return None
+        self.buckets[index].append(self.Entry(key, value))
+        self.count += 1
+        if self.count >= (2 / 3) * self.size:
+            self._resize()
+        return None
+        # raise NotImplementedError
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return self.count
+        # raise NotImplementedError
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
+        # raise NotImplementedError
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % self.size
+        # raise NotImplementedError
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        return ValuesIterator(self)
+        # raise NotImplementedError
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        return KeysIterator(self)
+        # raise NotImplementedError
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return ItemsIterator(self)
+        # raise NotImplementedError
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        items = [(key, value) for key, value in self.items()]
+        self.size *= 2
+        self.buckets = [[] for _ in range(self.size)]
+        self.count = 0
+        for element in items:
+            self.put(element[0], element[1])
+        # raise NotImplementedError
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        return "buckets: {}, items: {}".format(self.buckets, self.items())
+        # raise NotImplementedError
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        hash_item = self._get_hash(item)
+        index = self._get_index(hash_item)
+        for element in self.buckets[index]:
+            if element.get_key() == item:
+                return True
+        return False
+        # raise NotImplementedError
+
+
+class ValuesIterator:
+    def __init__(self, hashmap):
+        self.hashmap = hashmap
+        self.counter = 0
+        self.extra_counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.counter < self.hashmap.size:
+            while self.extra_counter < len(self.hashmap.buckets[self.counter]):
+                self.extra_counter += 1
+                return self.hashmap.buckets[self.counter][self.extra_counter - 1].get_value()
+            self.counter += 1
+            self.extra_counter = 0
+        raise StopIteration
+
+
+class KeysIterator:
+    def __init__(self, hashmap):
+        self.hashmap = hashmap
+        self.counter = 0
+        self.extra_counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.counter < self.hashmap.size:
+            while self.extra_counter < len(self.hashmap.buckets[self.counter]):
+                self.extra_counter += 1
+                return self.hashmap.buckets[self.counter][self.extra_counter - 1].get_key()
+            self.counter += 1
+            self.extra_counter = 0
+        raise StopIteration
+
+
+class ItemsIterator:
+    def __init__(self, hashmap):
+        self.hashmap = hashmap
+        self.counter = 0
+        self.extra_counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.counter < self.hashmap.size:
+            while self.extra_counter < len(self.hashmap.buckets[self.counter]):
+                self.extra_counter += 1
+                return (self.hashmap.buckets[self.counter][self.extra_counter - 1].get_key(),
+                        self.hashmap.buckets[self.counter][self.extra_counter - 1].get_value())
+            self.counter += 1
+            self.extra_counter = 0
+        raise StopIteration
