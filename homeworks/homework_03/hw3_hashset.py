b18727697a9065dc8a8fb7b037335b051bb06130
Макаров Максим <MisterMaks97@mail.ru>
2019-03-20 01:50:16 +0300

    ДЗ 3 HashSet не конец

--- homeworks/homework_03/hw3_hashset.py
+++ homeworks/homework_03/hw3_hashset.py
@@ -4,29 +4,41 @@
 from homeworks.homework_03.hw3_hashmap import HashMap
 
 
-class HashSet:
+class HashSet(HashMap):
 
     def __init__(self):
         # TODO Сделать правильно =)
-        raise NotImplementedError
+        super().__init__()
+        # raise NotImplementedError
 
     def get(self, key, default_value=None):
         # TODO достаточно переопределить данный метод
-        raise NotImplementedError
+        return super().__contains__(key)
+        # raise NotImplementedError
 
-    def put(self, key, value):
+    def put(self, key, value=None):
         # TODO метод put, нужно переопределить данный метод
-        raise NotImplementedError
+        super().put(key, value)
+        # raise NotImplementedError
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return super().__len__()
+        # raise NotImplementedError
 
     def values(self):
         # TODO возвращать итератор значений
-        raise NotImplementedError
+        return super().keys()
+        # raise NotImplementedError
 
     def intersect(self, another_hashset):
         # TODO метод, возвращающий новый HashSet
         #  элементы - пересечение текущего и другого
-        raise NotImplementedError
+        keys = [key for key in self.keys()]
+        another_keys = [key for key in another_hashset.keys()]
+        new_hashset = HashSet()
+        for key in keys:
+            if key in another_keys:
+                new_hashset.put(key)
+        return new_hashset
+        # raise NotImplementedError
