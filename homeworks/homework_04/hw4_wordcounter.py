f78e5dc8738c23e889b9226ed843dac86bd11317
Макаров Максим <MisterMaks97@mail.ru>
2019-03-25 23:50:39 +0300

    ДЗ 4 Wordcounter

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool, Queue
 import os
 
 
@@ -16,4 +16,26 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    result_dict = {}
+    total_count_words = 0
+    queue = Queue()
+    for filename in os.listdir(path_to_dir):
+        queue.put(filename)
+    with Pool(3) as p:
+        while not queue.empty():
+            filename = queue.get()
+            path_to_file = path_to_dir + "/" + filename
+            count_words = p.apply(count_word_in_file, (path_to_file,))
+            result_dict[filename] = count_words
+            total_count_words += count_words
+    result_dict["total"] = total_count_words
+    return result_dict
+    # raise NotImplementedError
+
+
+def count_word_in_file(filename):
+    count_words = 0
+    with open(filename, "r") as f:
+        for line in f:
+            count_words += len(line.split())
+    return count_words
