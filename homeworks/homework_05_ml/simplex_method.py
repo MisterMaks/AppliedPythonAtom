981f29cb3b6fda2e2445c69ab6edf4c62e2d03ef
Макаров Максим <MisterMaks97@mail.ru>
2019-04-09 04:34:41 +0300

    ДЗ 5 ML

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,20 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    size_of_a = a.shape
+    matrix = np.hstack((np.vstack((a, -c)),
+                        np.eye(size_of_a[0] + 1),
+                        np.vstack((b.reshape((size_of_a[0], 1)), 0))))
+    while any(matrix[-1, :] < 0):
+        true_col = matrix[-1, :].argmin()
+        true_stroka = (matrix[:-1, -1] / matrix[:-1, true_col]).argmin()
+        matrix[true_stroka, :] /= matrix[true_stroka, true_col]
+        for stroka in range(size_of_a[0] + 1):
+            if stroka != true_stroka:
+                matrix[stroka, :] -= matrix[true_stroka, :] * matrix[stroka, true_col]
+    x = np.zeros(size_of_a[1])
+    for col in range(size_of_a[1]):
+        if matrix[-1, col] == 0:
+            x[col] = matrix[matrix[:, col].argmax(), -1]
+    return x
+    # raise NotImplementedError
